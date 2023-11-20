6dbb84c140d75c50f26fe50d362bdfedd3218226
KEDIR NASIR <101452514+kedir21@users.noreply.github.com>
2023-11-20 15:25:08 +0300

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen text-6xl bg-red-800 text-white'>customers Page</div>
+  )
+}
+
+export default page
